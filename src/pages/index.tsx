1465ca9856b9928a9abf550ad2c46ada3f55bcce
Lucas Corte <corte1994.lc@gmail.com>
2021-05-10 18:32:30 -0300

    - instalação de dependencias - comeco da estilização

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link';
+import {Heading, Container} from '../styles/home';
+
+
 export default function Home() {
-  return <h1>Helo, world!</h1>;
+  return (
+    <Container>
+    <Heading>Home Page</Heading>
+    <Link href="/post">
+      <a className="post-link">Post</a>
+    </Link>
+    </Container>
+  );
 }
---
+++ src/styles/home.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  .post-link {
+    color: #f00;
+  }
+`;
+
+export const Heading = styled.h1`
+  font-size: 18px;
+  color: #999;
+`;
+
